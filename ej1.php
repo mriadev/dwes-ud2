9a782d2f6ac839b43ecb3a5fff5f840c44f5403c
Maria Cervilla <maria14998@gmail.com>
2022-09-26 09:12:36 +0200

    Update ej1.php

--- ej1.php
+++ ej1.php
@@ -1,3 +1,5 @@
 <?php 
-echo "'Hola mundo'";  
+echo "'Hola mundo'"; 
+echo '<br>';
+echo '<a href="https://github.com/mriadev/dwes-ud2/blob/main/ej1.php" target="_blank">GitHub Ejercicio1</a>';
 ?>
