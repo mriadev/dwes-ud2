8eb854b7fc07e1094a99f7248ee77e57ca5e2cc1
Maria Cervilla <maria14998@gmail.com>
2022-09-26 09:18:16 +0200

    Update ej1.php

--- ej1.php
+++ ej1.php
@@ -1,7 +1,9 @@
-<!-- Ejercicio 1. Script que muestre el mensaje Hola Mundo entrecomillado. -->
-
 <?php 
-echo "'Hola mundo'"; 
-echo '<br>';
-echo '<a href="https://github.com/mriadev/dwes-ud2/blob/main/ej1.php" target="_blank">GitHub Ejercicio1</a>';
+/**
+ * Ejercicio 1. Script que muestre el mensaje Hola Mundo entrecomillado.
+ * @author María Cervilla Alcalde
+ */
+echo "\"Hola mundo\""; 
 ?>
+<br>
+<a href="https://github.com/mriadev/dwes-ud2/blob/main/ej1.php" target="_blank">GitHub Ejercicio1</a>
